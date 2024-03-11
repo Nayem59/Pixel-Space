4756d09241ddc6bf1cf2bdf5fe71e945580b279b
Nayem59 <Nayem.Alam97@gmail.com>
2024-03-11 23:56:13 +0000

    added event listener for projectile

--- index.js
+++ index.js
@@ -24,6 +24,25 @@ class Player {
   }
 }
 
+// Create Projectile Class
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  // create a custom draw method that initiates a circul and fills it
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+}
+
 // Instantiate a Player
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -31,3 +50,9 @@ const player = new Player(x, y, 30, "blue");
 
 // call the draw method to show the player on screen
 player.draw();
+
+// add click eventlistener for projectile
+addEventListener("click", (e) => {
+  const projectile = new Projectile(e.clientX, e.clientY, 5, "red", null);
+  projectile.draw();
+});
