d2077fe0bb5c9bff1b525a08bf3194ce55f2586b
Nayem59 <Nayem.Alam97@gmail.com>
2025-01-03 22:18:32 +0000

    implemented sound volume adjustment

--- utils/sounds.js
+++ utils/sounds.js
@@ -5,14 +5,28 @@ class Sounds {
       projectileSound: "/../assets/soundEffects/shooting-sound.mp3",
       projectileSound2: "/../assets/soundEffects/shooting-sound2.wav",
       projectileSound3: "/../assets/soundEffects/shooting-sound3.wav",
+      projectileSound4: "/../assets/soundEffects/shooting-sound4.mp3",
+      projectileSound5: "/../assets/soundEffects/shooting-sound5.wav",
+      projectileSound6: "/../assets/soundEffects/shooting-sound6.mp3",
+      projectileSound7: "/../assets/soundEffects/shooting-sound7.mp3",
+      projectileSound8: "/../assets/soundEffects/shooting-sound8.mp3",
       explosion1: "/../assets/soundEffects/explosion1.wav",
       explosion2: "/../assets/soundEffects/explosion2.wav",
+      explosion3: "/../assets/soundEffects/explosion3.mp3",
+      explosion4: "/../assets/soundEffects/explosion4.mp3",
+      explosion5: "/../assets/soundEffects/explosion5.mp3",
+      explosion6: "/../assets/soundEffects/explosion6.mp3",
+      explosion7: "/../assets/soundEffects/explosion7.wav",
+      explosion8: "/../assets/soundEffects/explosion8.wav",
+      explosion9: "/../assets/soundEffects/explosion9.wav",
+      laser: "/../assets/soundEffects/laser.mp3",
     };
 
     // Container for all loaded sounds
     // e.g., { laser: { audio: (AudioEl), isLoaded: true } }
     this.sounds = {};
     this.loadingPromises = [];
+    this.defaultVolume = 1.0;
 
     // Preload each sound
     Object.keys(this.toLoad).forEach((key) => {
@@ -36,16 +50,29 @@ class Sounds {
   }
 
   // Play a sound by its key
-  playSound(key) {
+  playSound(key, volume = this.defaultVolume) {
     if (this.sounds[key] && this.sounds[key].isLoaded) {
       //   this.sounds[key].audio.currentTime = 0; // Reset to the start
       //   this.sounds[key].audio.play();
 
       // Clone the audio element for simultaneous playback
       const audioClone = this.sounds[key].audio.cloneNode();
+      audioClone.volume = volume;
       audioClone.play();
     } else {
       console.warn(`Sound "${key}" is not loaded or doesn't exist.`);
+    }
+  }
+
+  setDefaultVolume(volume) {
+    if (volume >= 0 && volume <= 1) {
+      this.defaultVolume = volume;
+      // Update all preloaded audio elements
+      Object.values(this.sounds).forEach((sound) => {
+        sound.audio.volume = volume;
+      });
+    } else {
+      console.error("Volume must be between 0.0 and 1.0.");
     }
   }
 
